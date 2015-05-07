bf9401de1f3eb25918c80e0f3298fcbfa6b11971
Nathan Barreto <nathanmrtns@gmail.com>
2015-05-06 21:43:52 -0500

    Add a clock and background

--- index.php
+++ index.php
@@ -5,4 +5,9 @@ include('header.html');
 
 ?>
 
-<h1>Welcome to the Hall Management System</h1>
+<h2>Hall Management System</h2>
+
+
+<div style="position: relative; top: 500px; left: 770px;">
+	<div style="margin: 15px 0px 0px; display: inline-block; text-align: center; width: 392px;"><div style="display: inline-block; padding: 2px 4px; margin: 0px 0px 5px; border: 1px solid rgb(204, 204, 204); text-align: center; background-color: transparent;"><a href="http://localtimes.info/North_America/United_States/North_Dakota/Fargo/" style="text-decoration: none; font-size: 13px; color: rgb(0, 0, 0);"><img src="http://localtimes.info/images/countries/us.png"="" border=0="" style="border:0;margin:0;padding:0"=""> Fargo</a></div><script type="text/javascript" src="http://localtimes.info/clock.php?continent=North America&country=United States&province=North Dakota&city=Fargo&cp1_Hex=000000&cp2_Hex=FFFFFF&cp3_Hex=000000&fwdt=392&ham=0&hbg=1&hfg=1&sid=0&mon=0&wek=0&wkf=0&sep=0&widget_number=1024"></script></div>
+</div>
